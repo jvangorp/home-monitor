627021535676814a99422e0ef4802080f7581f27
John Van Gorp <john.vangorp@gmail.com>
2015-06-02 06:28:07 -0700

    Complete rewrite to query db directly.

--- eagle-watchdog.py
+++ eagle-watchdog.py
@@ -1,7 +1,5 @@
-# Receive Eagle gateway messages from the RabbitMQ queue and
-# restart the web endpoint when there is a loss of comms from the gateway.
-import pika
-from lxml import etree
+# Get the latest kW demand record from the Postgres database and
+# restart the web endpoint if the timestamp is too old.
 import logging
 import time
 from ConfigParser import SafeConfigParser
@@ -14,45 +12,23 @@ database = config.get('Postgres', 'database')
 user = config.get('Postgres', 'user')
 password = config.get('Postgres', 'password')
 
-# Set up the connection to the RabbitMQ server.
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
-channel = connection.channel()
-
-# Set up the link to the selected RabbitMQ exchange.
-channel.exchange_declare(exchange='vangorp.home.eagle',
-                         type='fanout')
-
-# Grab a queue name and bind it to the exchange.
-result = channel.queue_declare(exclusive=True)
-queue_name = result.method.queue
-
-channel.queue_bind(exchange='vangorp.home.eagle',
-                   queue=queue_name)
-
 # Set up Python logging defaults
-logging.basicConfig(filename='eagle-endpoint.log')
+logging.basicConfig(filename='eagle-watchdog.log')
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
-# Set up callback function to process queue messages.
-def callback(ch, method, properties, body):
-    # print " [x] %r" % (body,)
-    message = etree.fromstring(body)
+# Set up connection to Postgres database.
+conn_string = """
+host={0} dbname={1} user={2} password={3}
+""".format(host, database, user, password)
 
-    # Extract timestamp and remove the 's' at the end.
-    eagle_time = message.attrib['timestamp']
-    eagle_time = int(eagle_time[:-1])
+conn = psycopg2.connect(conn_string)
+cursor = conn.cursor()
 
-    # Compare Eagle gateway time with server time - if the difference
-    # is too large, restart the 'eagle-endpoint.py' script and
-    # log an error.
-    server_time = int(time.time())
-    print(abs(server_time - eagle_time))
+# Get the latest kW demand timestamp (in epoch form) from Postgres.
+query = "select extract(epoch from ts) from demand order by ts desc limit 1;"
+cursor.execute(query)
+latest_timestamp = cursor.fetchone()
 
+# Compare latest kW demand timestamp against current time.
+# time_delta = abs()
 
-
-channel.basic_consume(callback,
-                      queue=queue_name,
-                      no_ack=True)
-
-channel.start_consuming()
